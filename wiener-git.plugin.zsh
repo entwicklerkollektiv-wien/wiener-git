b0b052021e2d01bb37f3420b87b4f4947b269335
XI <cmoser@xivps.xyz>
2023-07-17 13:52:29 +0200

    Änder bissi was, Dua vüh dazua (#10)
    Des schoafe ß is ned so leiwand zum schreibn auf ana us tastatur.
    Die `muss` zum `muas` is geschmack.
    Da rest sind amal ideen, I muas söba no schaun wie guat des
    funktioniert.

--- wiener-git.plugin.zsh
+++ wiener-git.plugin.zsh
@@ -9,31 +9,58 @@ function wiener-git() {
             # basic snapshotting
             gibdazua) git_command+=(add);;
             wostuatsi) git_command+=(status);;
+            wosisaunderst) git_command+=(diff);;
             nimmdes) git_command+=(commit);;
             --hobivagessn) git_command+=(--amend);;
             --passtscho) git_command+=(--no-verify);;
+            zetterl) git_command+=(notes);;
+            upsala) git_command+=(restore);;
+            scheisdrauf) git_command+=(reset);;
+            hausweg) git_command+=(rm);;
+            hausumi) git_command+=(mv);;
             # branching and merging
             astl) git_command+=(branch);;
             gehumi) git_command+=(checkout);;
             --astl) git_command+=(-b);;
+            schoitum) git_command+=(switch);;
+            mochagulasch) git_command+=(merge);;
+            gulaschpfandl) git_command+=(mergetool);;
             gschichtl) git_command+=(log);;
             wegmitdemschaß) git_command+=(stash);;
             retour) git_command+=(pop);;
+            merkdades) git_command+=(tag);;
+            --kraxn) git_command+=(--sign);;
+            --kanekraxn) git_command+=(--no-sign);;
+            bam) git_command+=(worktree);;
             # sharing and updating projects
             schaumaamoi) git_command+=(fetch);;
             --ois) git_command+=(--all);;
             gibmades) git_command+=(pull);;
             dahost) git_command+=(push);;
-            --ischeißaufdi) git_command+=(--force);;
+            --ischeisaufdi) git_command+=(--force);;
+            address) git_command+=(remote);;
+            neich) git_command+=(add);;
+            ondare) git_command+=(rename);;
+            wegdamit) git_command+=(remove);;
+            schublade) git_command+=(submodule);;
+            packdizam) git_command+=(archive);;
             # inspection and comparison
+            zeigsma) git_command+=(show);;
+            kuazesgschichtl) git_command+=(shortlog);;
+            dazoeh) git_command+=(describe);;
             # patching 
+            hausdazua) git_command+=(apply);;
             rosinenpickn) git_command+=(cherry-pick);;
             schiabsdrunta) git_command+=(rebase);;
-            --gehweida) git_command+=(--continue);;
+            --duaweida) git_command+=(--continue);;
             --naoida) git_command+=(--abort);;
-            --domussiwosaendan) git_command+=(--interactive);;
+            --domuasiwosaendan) git_command+=(--interactive);;
+            vagisswieda) git_command+=(revert);;
+            --oetan) git_command+=(--mainline);;
             # debugging
+            wowoades) git_command+=(bisect);;
             weawoas) git_command+=(blame);;
+            suachdes) git_command+=(grep);;
             *) git_command+=($arg);;
 
         esac
